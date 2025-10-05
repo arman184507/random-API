50d4059aa4d188fbeb763c408d0438d6cb529734
Shahid <23it09.arman@pccegoa.edu.in>
2025-10-06 01:18:23 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import {  useState, useEffect } from "react";
+import {getPosts, getRandomUser } from  './api';
+import PostCard from "./components/PostCard";
+import UserCard from "./components/UserCard";
+import './App.css';
+
+function App() {
+  const [data, setData] = useState([]);
+  const [userData, setUserData] = useState(null);
+  useEffect(() => {
+    getPosts().then((posts) => setData(posts));
+  }, []);
+
+  useEffect (() => {
+    getRandomUser().then((user) => setUserData(user.results[0]));
+  }, []);
+
+  const refresh = () => {
+    getRandomUser().then((user) => setUserData(user.results[0]));
+  };
+  return (
+    <div className="App">
+      {userData && <UserCard data={userData} />}
+      <button onClick={refresh}>Refresh User</button>
+      {
+        data ? data.map(e => <PostCard title={e.title} body={e.body} /> ) : <p>No Data</p> 
+      }
+    </div>
+  );
+}
+
+export default App;
